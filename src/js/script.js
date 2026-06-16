af3824284317e74c5d4e7bb3f0ccbb68d2b58b63
denisalecexe <deniszenomiteus@gmail.com>
2026-06-16 18:19:09 +0200

    variables javascript and console log for test

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// VARIABILI JS
+// variabile che si aggancia al timer per poterlo fare funzionare
+const displayTimer = document.getElementById("timer");
+
+// variabili per agganciarsi ai bottoni principali per far partire o fermare il timer
+const btnStart = document.getElementById("start");
+const btnPause = document.getElementById("pause");
+const btnRestart = document.getElementById("reset");
+
+
+
+// TEST DI FUNZIONAMENTO
+console.log("JavaScript funziona!");
